f5e3b2da2e750cff4c0ea722255647e0e046bcb6
charlesnyankieya <nyankieyao@gmail.com>
2024-03-12 16:37:29 +0300

    Adds list comprehensions to the library

---
+++ list_comprehensions.py
@@ -0,0 +1,11 @@
+nums = [12, 64, 98, 17, -13]
+double = []
+
+for num in nums:
+    double.append(num * 2)
+    print(double)
+
+numbers = [2, 6, 12, -16, 18]
+doubled = [number * 2 for number in numbers]
+print(doubled)
+
